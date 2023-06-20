409e890f18ef968ec1588b7dfe53212ca01d5261
Inna <inna@z-website.ru>
2023-06-20 14:43:20 +0300

    Gutenberg Block Layout

--- functions.php
+++ functions.php
@@ -15,3 +15,59 @@ function hotel_company_enqueue_styles() {
 /*
  * Your code goes below
  */
+
+// --- New Gutenberg Block Layout Codeweber---
+if (!function_exists('checkCategoryOrder')) {
+    function checkCategoryOrder($categories)
+    {
+        //custom category array
+        $temp = array(
+            'slug'  => 'codeweber',
+            'title' => 'Codeweber Blocks'
+        );
+        $temp_1 = array(
+            'slug'  => 'codeweber_elements',
+            'title' => 'Codeweber Elements'
+        );
+        $temp_2 = array(
+            'slug'  => 'child_blocks',
+            'title' => 'Child Blocks'
+        );
+        //new categories array and adding new custom category at first location
+        $newCategories = array();
+        $newCategories[0] = $temp;
+        $newCategories[1] = $temp_1;
+        $newCategories[2] = $temp_2;
+        //appending original categories in the new array
+        foreach ($categories as $category) {
+            $newCategories[] = $category;
+        }
+        //return new categories
+        return $newCategories;
+    }
+}
+add_filter('block_categories_all', 'checkCategoryOrder', 99, 1);
+
+
+
+// --- ACF Flexible Block
+add_action('acf/init', 'my_acf_blocks_init_child');
+function my_acf_blocks_init_child()
+{
+
+    // Register Child blocks.
+    acf_register_block_type(array(
+        'name'              => 'childs_blocks',
+        'title'             => __('Child blocks'),
+        'description'       => __('Child blocks flexible block.'),
+        'render_template'   => 'templates/flexible-content/child_blocks.php',
+        'category'          => 'child_blocks',
+        'align'           => 'full',
+        'supports'        => array(
+            'align'        => array('full'),
+            'align'        => true,
+        ),
+        'mode' => 'preview',
+
+    ));
+};
